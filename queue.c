03ac164efa10714968067f047eec8e947c37622b
ddoyon7 <ddoyon@korea.ac.kr>
2025-05-08 22:56:12 +0900

    Ready Queue 구현 ( 힙 구조로 변경 )

--- queue.c
+++ queue.c
@@ -1,17 +1,15 @@
 #include "queue.h"
 
 process_info* ready_queue[QUEUE_SIZE];
-int rq_front, rq_rear;
-
-void Config() {
-	rq_front = rq_rear = -1;
-}
-
-//priority나 그냥 을 둘다 쓸 수있는걸로 만들어야되네 
+int rq_num;
 
 void Push_Ready_Queue(process_info* pi, int type) {
 	if (Is_Full_QUEUE()) printf("ERR_QUEUE IS FULL\n");
-	else ready_queue[++rq_rear] = pi;
+	else {
+		ready_queue[rq_num] = pi;
+		Heapify_Up(rq_num, type);
+		rq_num++;
+	}
 }
 
 process_info* Pop_Ready_Queue(int type) {
@@ -19,30 +17,81 @@ process_info* Pop_Ready_Queue(int type) {
 		printf("ERR_QUEUE IS EMPTY\n");
 		return NULL;
 	}
-	return ready_queue[++rq_front];
-}
-
-int Is_Empty_QUEUE() {
-	return rq_front == rq_rear;
-}
-
-int Is_Full_QUEUE() {
-	return rq_rear == QUEUE_SIZE - 1;
-}
-
-void Heapify_Up(int type) {
-	if (rq_rear == 0) return;
-	int parent = (rq_rear - 1) / 2;
-
-
-	if (heap->data[parent] > heap->data[index]) {
-		swap(&heap->data[parent], &heap->data[index]);
-		heapify_up(heap, parent);
+	else {
+		process_info* res = ready_queue[0];
+		ready_queue[0] = ready_queue[--rq_num];
+		Heapify_Down(0, type);
+		return res;
 	}
 }
 
-void Heapify_Down(int type) {
+int Is_Empty_QUEUE() {
+	return rq_num == 0;
+}
 
+int Is_Full_QUEUE() {
+	return rq_num == QUEUE_SIZE;
+}
+
+void Heapify_Up(int idx, int type) {
+	if (idx == 0) return;
+	int parent = (rq_num - 1) / 2;
+
+	switch (type) {
+	case T_SJF:
+		if (ready_queue[parent]->cpu_burst_time > ready_queue[idx]->cpu_burst_time) {
+			Swap_ReadyQueue(parent, idx);
+			Heapify_Up(parent, type);
+		}
+		break;
+
+	case T_PR:
+		if (ready_queue[parent]->priority > ready_queue[idx]->priority) { // 낮을수록 우선순위가 높다.
+			Swap_ReadyQueue(parent, idx);
+			Heapify_Up(parent, type);
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+void Heapify_Down(int idx, int type) {
+	int left = 2 * idx + 1;
+	int right = 2 * idx + 2;
+	int target = idx;
+
+	switch (type) {
+	case T_FCFS:
+		if (left < rq_num && ready_queue[left]->arrival_time < ready_queue[target]->arrival_time)
+			target = left;
+		if (left < rq_num && ready_queue[right]->arrival_time < ready_queue[target]->arrival_time)
+			target = right;
+		break;
+
+	case T_SJF:
+		if (left < rq_num && ready_queue[left]->cpu_burst_time < ready_queue[target]->cpu_burst_time)
+			target = left;
+		if (left < rq_num && ready_queue[right]->cpu_burst_time < ready_queue[target]->cpu_burst_time)
+			target = right;
+		break;
+
+	case T_PR:
+		if (left < rq_num && ready_queue[left]->priority < ready_queue[target]->priority)
+			target = left;
+		if (left < rq_num && ready_queue[right]->priority < ready_queue[target]->priority)
+			target = right;
+		break;
+
+	default:
+		break;
+	}
+
+	if (target != idx) {
+		Swap_ReadyQueue(target, idx);
+		Heapify_Down(target, type);
+	}
 }
 
 void Swap_ReadyQueue(int idx1, int idx2) {
--- queue.h
+++ queue.h
@@ -2,12 +2,13 @@
 #define __QUEUE_H__
 #include "process.h"
 
+enum SCHEDULE_TYPE { T_FCFS, T_SJF, T_PR, T_RR, T_PSJF, T_PPR };
 #define QUEUE_SIZE 100
 
 extern process_info* ready_queue[QUEUE_SIZE];
-extern int rq_front, rq_rear;
+extern int rq_num;
 
-void Config();
+//void Config();
 
 void Push_Ready_Queue(process_info*, int);
 
@@ -17,9 +18,9 @@ int Is_Empty_QUEUE();
 
 int Is_Full_QUEUE();
 
-void Heapify_Up(int);
+void Heapify_Up(int, int);
 
-void Heapify_Down(int);
+void Heapify_Down(int, int);
 
 void Swap_ReadyQueue(int, int);
 //process_info waiting_queue[QUEUE_SIZE];
