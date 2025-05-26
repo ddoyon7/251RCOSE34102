4c1e73f16f6647b3356476a4125786c310d06dfd
ddoyon7 <ddoyon@korea.ac.kr>
2025-05-26 23:35:58 +0900

    디버그 코드 수정

--- process.c
+++ process.c
@@ -22,15 +22,16 @@ void Create_Process() {
 		process_list[i].cpu_burst_time = rand() % MAX_CPUBURSTTIME + 1;
 		process_list[i].priority = rand() % MAX_PRIORITY + 1;
 		process_list[i].io_burst_time = rand() % MAX_IOBURSTTIME + 1;
+		process_list[i].period = MAX_CPUBURSTTIME + 5 * (rand() % MAX_PERIOD);
 		for (int j = 0; j < IOREQUEST_NUMBER; j++) process_list[i].io_request_time[j] = rand() % (MAX_IOREQUESTTIME + 1);
 	}
-	// debug code
+	/* debug code*/
 	/*process_info temp_list[PROCESS_NUMBER] = {
-		{12997,3,1,5,{31,19,4},1},
-		{21805,7,16,2,{31,47,29},3},
-		{9414,7,1,4,{33,37,5},5},
-		{30538,18,20,3,{4,35,44},2},
-		{18573,20,10,2,{8,32,30},2}
+		{1,3,25,5,{31,19,4},1, 50},
+		{2,7,35,4,{33,37,5},5 ,80},
+		{21805,8,30,2,{31,47,29}, 3, 100},
+		{30538,18,20,3,{4,35,44},2,30},
+		{18573,20,10,2,{8,32,30},2,30}
 	};
 	for (int i = 0; i < PROCESS_NUMBER; i++) {
 		process_list[i].pid = temp_list[i].pid;
@@ -38,18 +39,20 @@ void Create_Process() {
 		process_list[i].cpu_burst_time = temp_list[i].cpu_burst_time;
 		process_list[i].priority = temp_list[i].priority;
 		process_list[i].io_burst_time = temp_list[i].io_burst_time;
+		process_list[i].period = temp_list[i].period;
 		for (int j = 0; j < IOREQUEST_NUMBER; j++) process_list[i].io_request_time[j] = temp_list[i].io_request_time[j];
 	}*/
 	qsort(process_list, PROCESS_NUMBER, sizeof(process_info), CMP);
+	printf("5 Processes Created.");
 }
 
 /*
 * 생성된 process의 정보를 출력한다.
 */
 void Show_Process() {
-	printf("---------------------------------------------------< created process >-------------------------------------------\n");
-	for (int i = 0; i < PROCESS_NUMBER; i++) printf("Process %5d | ArrivalTime : %2d, CpuBurstTime : %2d, Priority : %2d, IoBurstTime : %2d, IoRequestTime : %2d, %2d, %2d\n", process_list[i].pid, process_list[i].arrival_time, process_list[i].cpu_burst_time, process_list[i].priority, process_list[i].io_burst_time, process_list[i].io_request_time[0], process_list[i].io_request_time[1], process_list[i].io_request_time[2]);
-	printf("-----------------------------------------------------------------------------------------------------------------\n");
+	printf("--------------------------------------------------------< Created Processes >------------------------------------------------\n");
+	for (int i = 0; i < PROCESS_NUMBER; i++) printf("Process %5d | ArrivalTime : %2d, CpuBurstTime : %2d, Priority : %2d, IoBurstTime : %2d, IoRequestTime : %2d, %2d, %2d, Period : %2d\n", process_list[i].pid, process_list[i].arrival_time, process_list[i].cpu_burst_time, process_list[i].priority, process_list[i].io_burst_time, process_list[i].io_request_time[0], process_list[i].io_request_time[1], process_list[i].io_request_time[2], process_list[i].period);
+	printf("-----------------------------------------------------------------------------------------------------------------------------\n");
 }
 
 /*
