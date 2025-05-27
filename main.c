844245f6335287d0ae6d3239b49059fb59a33999
ddoyon7 <ddoyon@korea.ac.kr>
2025-05-27 16:51:02 +0900

    CLI 수정

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include "scheduler.h"
 
+/*
+* --------------------------------
+* < CPU SCHEDULING SIMULATOR CLI >
+* --------------------------------
+*/
 int main() {
 	system("clear");
 	srand(time(NULL));
 	int input;
 	while (1) {
 		printf("< CPU SCHEDULING SIMULATOR CLI >\n");
-		printf("1. Create_Process 2. Show_Process\n3. FirstComeFirstServed 4. ShortestJobFirst 5. Priority 6. RoundRobin 7. Preemptive SJF 8. Preemptive Priority\n9. Rate Monotonic (with two processes) 10. Earliest Deadline First (with two processes)\n11. Evaluation\n\nInput any other number to exit\n\n");
+		printf("1. Create_Process 2. Show_Process\n3. FirstComeFirstServed 4. ShortestJobFirst 5. Priority 6. RoundRobin 7. Preemptive SJF 8. Preemptive Priority\n9. Rate Monotonic 10. Earliest Deadline First (Realtime scheduling with two processes, Arrived at 0 and no IO Request)\n11. Evaluation (FCFS, SJF, Priority, RR, Preemptive SJF, Preemptive Priority)\n\nInput any other number to exit\n\n");
 		printf(">> ");
 		if (scanf("%d", &input) == -1) return 0;
 		system("clear");
